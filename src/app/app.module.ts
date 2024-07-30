2f6b79769e0d21307527beb6946d4d4119076201
Alana Maia <alanaalvesmaia@gmail.com>
2024-07-30 13:27:18 -0300

    migrate the whole project to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent], // is for non standalone components
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ], // is for non standalone components
   bootstrap: [AppComponent], // the root component
   imports: [
     // is for standalone components
     BrowserModule,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
+    NgFor,
+    NgIf,
+    FormsModule,
+    SharedModule,
   ],
 })
 export class AppModule {}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NgFor, NgIf } from '@angular/common';
-import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NgFor, NewTaskComponent, NgIf],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
